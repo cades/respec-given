10a0cb2c9ccf9ad97516fcdc6e35c2543c1ee0aa
cades kao <cadeskao@gmail.com>
2016-06-10 14:06:44 +0800

    refactor code

--- enable-natural-assertion/transform-then.js
+++ enable-natural-assertion/transform-then.js
@@ -7,47 +7,60 @@ module.exports = function(source, filepath) {
   var ast = esprima.parse(source)
   estraverse.traverse(ast, {
     enter: function(node, parent) {
-      if (node.type === 'CallExpression' && node.callee.name === 'Then') {
-        var getters = [],
-            fnAst = node.arguments.find(isFunctionExpression)
+      var fnAst
+      if (node.type !== 'CallExpression') return
+      if (node.callee.name !== 'Then') return
+      if (!(fnAst = node.arguments.find(isFunctionExpression))) return
 
-        if (!fnAst) return
-        if (useAsyncStyle()) return // this should be forbidden
+      createGetterAsExtraArgument(fnAst, node)
 
-        if (!bodyContainsOnlyOneStatement()) return
-        if (!theStatementIsReturnStatement()) return
-
-        estraverse.traverse(returnedExpression(), {
-          enter: function(node, parent) {
-            if (nodeIsNotAnIdentifier()) return
-            if (nodeIsPropertyOfOtherIdentifier()) return
-            getters.push(createGetterAst(node.name))
-
-            function nodeIsNotAnIdentifier() { return node.type !== 'Identifier' }
-            function nodeIsPropertyOfOtherIdentifier() {
-              return parent.type === 'MemberExpression' && node === parent.property
-            }
-          }
-        })
-
-        appendGettersToArgumentList()
-
-        function isFunctionExpression(node) { return node.type.match(/(Arrow)?FunctionExpression/) }
-        function useAsyncStyle() { return  fnAst.params.length > 0 }
-        function bodyContainsOnlyOneStatement() { return statements().length === 1 }
-        function statements() { return fnAst.body.body }
-        function theStatementIsReturnStatement() { return firstStatement().type === 'ReturnStatement' }
-        function returnedExpression() { return firstStatement().argument }
-        function firstStatement() { return statements()[0] }
-        function appendGettersToArgumentList() {
-          node.arguments.push({
-            "type": "ObjectExpression",
-            "properties": getters
-          })
-        }
+      function isFunctionExpression(node) {
+        return node.type.match(/(Arrow)?FunctionExpression/)
       }
     }
   })
+
+  function createGetterAsExtraArgument(fnAst, callAst) {
+    var getters = []
+
+    if (useAsyncStyle()) return // this should be forbidden
+    if (!bodyContainsOnlyOneStatement()) return
+    if (!theStatementIsReturnStatement()) return
+
+    estraverse.traverse(returnedExpression(), {
+      enter: function(node, parent) {
+        if (nodeIsNotAnIdentifier()) return
+        if (nodeIsPropertyOfOtherIdentifier()) return
+
+        createGetterForLexicalVariable(node.name)
+
+        function nodeIsNotAnIdentifier() { return node.type !== 'Identifier' }
+        function nodeIsPropertyOfOtherIdentifier() {
+          return parent.type === 'MemberExpression' && node === parent.property
+        }
+      }
+    })
+
+    appendGettersToArgumentList()
+
+    function useAsyncStyle() { return  fnAst.params.length > 0 }
+    function bodyContainsOnlyOneStatement() { return statements().length === 1 }
+    function statements() { return fnAst.body.body }
+    function theStatementIsReturnStatement() {
+      return firstStatement().type === 'ReturnStatement'
+    }
+    function returnedExpression() { return firstStatement().argument }
+    function firstStatement() { return statements()[0] }
+    function createGetterForLexicalVariable(varname) {
+      getters.push(createGetterAst(varname))
+    }
+    function appendGettersToArgumentList() {
+      callAst.arguments.push({
+        "type": "ObjectExpression",
+        "properties": getters
+      })
+    }
+  }
 
   return escodegen.generate(ast)
 }
