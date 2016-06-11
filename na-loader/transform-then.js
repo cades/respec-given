7dafc23bbe1424a334b53d51d3613bc04686cfb5
cades kao <cadeskao@gmail.com>
2016-06-11 22:44:18 +0800

    bugfix: attribute typo
    right -> rigt. this is scary and debug it is time-consuming.

--- na-loader/transform-then.js
+++ na-loader/transform-then.js
@@ -64,7 +64,7 @@ module.exports = function(source, filepath) {
             evaluators: [],
             isBinaryExpression: isBinaryExpression,
             left: function() { return },
-            rigt: function() { return },
+            right: function() { return },
             filepath: filepath,
             loc: returnedExpression().loc
           }
